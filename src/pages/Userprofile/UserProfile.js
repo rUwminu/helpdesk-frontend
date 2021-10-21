498fa93dc61372f3a914ce91f1aab92bc0d05e61
rUwminu <rayigo98@gmail.com>
2021-10-21 17:11:19 +0800

    update

--- src/pages/Userprofile/UserProfile.js
+++ src/pages/Userprofile/UserProfile.js
@@ -3,13 +3,17 @@ import tw from "twin.macro";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { gql, useQuery } from "@apollo/client";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 // Redux
 import { getSingleUser } from "../../redux/action/userAction";
 
 //Icon class
-import { MarkEmailRead, AdminPanelSettings } from "@mui/icons-material";
+import {
+  MarkEmailRead,
+  AdminPanelSettings,
+  ChevronLeft,
+} from "@mui/icons-material";
 
 const UserProfile = () => {
   const { id } = useParams();
@@ -42,53 +46,65 @@ const UserProfile = () => {
   return (
     <SectionContainer>
       <Container>
-        <h1 className="title">User Profile</h1>
+        <div className="title-container">
+          <h1 className="title">User Profile</h1>
+          <Link to="/helpdesk-frontend/user_panel" className="back-btn">
+            <ChevronLeft className="back-icon" />
+            To Home
+          </Link>
+        </div>
+
         <div className="info">
           {userInfo && (
-            <LeftContainer>
-              <div className="user-title">
-                <div className="logo">
-                  {getFirstCharaterOfUsername(userInfo.username)}
+            <>
+              <LeftContainer>
+                <div className="user-title">
+                  <div className="logo">
+                    {getFirstCharaterOfUsername(userInfo.username)}
+                  </div>
+                  <div className="user-info">
+                    <h2>{userInfo.username}</h2>
+                    <small>{userInfo.department}</small>
+                  </div>
                 </div>
-                <div className="user-info">
-                  <h2>{userInfo.username}</h2>
-                  <small>{userInfo.department}</small>
+                <div className="user-detail">
+                  <h1>User Information</h1>
+                  <div className="list-items">
+                    <MarkEmailRead className="icon mail" />
+                    <h2>{userInfo.email}</h2>
+                  </div>
+                  <div className="list-items">
+                    <AdminPanelSettings className="icon admin" />
+                    <h2>
+                      {userInfo.isAdmin ? "Admin Permission" : "Normal User"}
+                    </h2>
+                  </div>
                 </div>
-              </div>
-              <div className="user-detail">
-                <h1>User Information</h1>
-                <div className="list-item">
-                  <MarkEmailRead className="icon mail" />
-                  <h2>{userInfo.email}</h2>
+              </LeftContainer>
+
+              <RightContainer>
+                <h1>Edit user Profile</h1>
+                <div className="input-item">
+                  <h2>Username</h2>
+                  <input placeholder={userInfo.username} />
                 </div>
-                <div className="list-item">
-                  <AdminPanelSettings className="icon admin" />
-                  <h2>
-                    {userInfo.isAdmin ? "Admin Permission" : "Normal User"}
-                  </h2>
+                <div className="input-item">
+                  <h2>Email</h2>
+                  <input placeholder={userInfo.email} />
                 </div>
-              </div>
-            </LeftContainer>
+                <div className="input-item">
+                  <h2>Password</h2>
+                  <input placeholder="Atleast 6 Charater" />
+                </div>
+                <div className="input-item">
+                  <h2>Comfirm Password</h2>
+                  <input placeholder="Retype Your Password" />
+                </div>
+
+                <div className="btn">Update</div>
+              </RightContainer>
+            </>
           )}
-          <RightContainer>
-            <h1>Edit user Profile</h1>
-            <div className="input-item">
-              <h2>Username</h2>
-              <input placeholder={userInfo.username} />
-            </div>
-            <div className="input-item">
-              <h2>Username</h2>
-              <input placeholder={userInfo.username} />
-            </div>
-            <div className="input-item">
-              <h2>Username</h2>
-              <input placeholder={userInfo.username} />
-            </div>
-            <div className="input-item">
-              <h2>Username</h2>
-              <input placeholder={userInfo.username} />
-            </div>
-          </RightContainer>
         </div>
       </Container>
     </SectionContainer>
@@ -130,6 +146,56 @@ const Container = styled.div`
     md:max-w-5xl 
   `}
 
+  .title-container {
+    ${tw`
+        flex
+        items-center
+        justify-between
+    `}
+
+    .back-btn {
+      ${tw`
+        flex
+        items-center
+        justify-center
+        py-1
+        pr-3
+        md:text-lg
+        text-gray-200
+        font-semibold
+        rounded-md
+        transition
+        duration-200
+        ease-in-out
+        overflow-hidden
+      `}
+
+      .back-icon {
+        ${tw`
+          relative
+          text-xl
+          md:text-3xl
+          transition
+          duration-200
+          ease-in-out
+        `}
+      }
+
+      :hover {
+        ${tw`
+          bg-gray-700
+        `}
+
+        .back-icon {
+          ${tw`
+             
+          `}
+          animation: backAnimate 2.5s ease infinite;
+        }
+      }
+    }
+  }
+
   .title {
     ${tw`
         mb-4
@@ -149,6 +215,49 @@ const Container = styled.div`
         justify-start
         md:justify-between
     `}
+  }
+
+  @keyframes backAnimate {
+    20% {
+      ${tw`
+        opacity-0
+        -translate-x-full
+      `}
+    }
+    30% {
+      ${tw`
+        opacity-0
+        translate-x-full
+      `}
+    }
+    40% {
+      ${tw`
+        opacity-100
+        translate-x-0
+        left-1
+      `}
+    }
+    50% {
+      ${tw`
+        left-0
+      `}
+    }
+    55% {
+      ${tw`
+        left-1
+      `}
+    }
+    60% {
+      ${tw`
+        left-0
+      `}
+    }
+    100% {
+      ${tw`
+        left-0
+        translate-x-0
+      `}
+    }
   }
 `;
 
@@ -218,7 +327,7 @@ const LeftContainer = styled.div`
       `}
     }
 
-    .list-item {
+    .list-items {
       ${tw`
         ml-2
         mb-2
@@ -234,11 +343,15 @@ const LeftContainer = styled.div`
       }
 
       .mail {
-        ${tw``}
+        ${tw`
+            text-blue-500
+        `}
       }
 
       .admin {
-        ${tw``}
+        ${tw`
+            text-red-500
+        `}
       }
 
       h2 {
@@ -267,6 +380,50 @@ const RightContainer = styled.div`
         font-semibold
         text-gray-200
     `}
+  }
+
+  .input-item {
+    ${tw`
+        mb-4
+        flex
+        flex-col
+    `}
+
+    h2 {
+      ${tw`
+        text-lg
+        text-gray-200
+      `}
+    }
+
+    input {
+      ${tw`
+        py-1
+        px-2
+        w-full
+        md:max-w-xs
+        border-b
+        text-gray-300
+        bg-transparent
+        focus:outline-none
+      `}
+    }
+  }
+
+  .btn {
+    ${tw`
+        flex
+        items-center
+        justify-center
+        py-1
+        w-full
+        md:max-w-[14rem]
+        text-lg
+        bg-blue-700
+        text-gray-200
+        font-semibold
+        rounded-sm
+      `}
   }
 `;
 
